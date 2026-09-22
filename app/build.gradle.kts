b73468b822f335e1cd68bab5eda25796b6ef1792
zhyang18 <zhyang18@163.com>
2026-09-22 16:13:14 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.battery.analysis"
         minSdk = 24
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.9.8.6"
+        versionCode = 43
+        versionName = "1.9.8.7"
 
         externalNativeBuild {
             cmake {

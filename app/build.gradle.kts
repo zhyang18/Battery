527f3307cb596e6296a654cae62f5ab01b043bc6
zhyang18 <zhyang18@163.com>
2026-09-24 17:20:18 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.battery.analysis"
         minSdk = 24
         targetSdk = 36
-        versionCode = 47
-        versionName = "1.9.9"
+        versionCode = 48
+        versionName = "1.9.9.1"
 
         externalNativeBuild {
             cmake {
